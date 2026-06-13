145fbf1ab261f510df0503ebfda8a83ed1855188
Shreya_Pathak <Pathakshreyab@gmail.com>
2026-06-13 09:15:19 +0530

    Delete Seperate_the_digit_in_an_array.py

--- Seperate_the_digit_in_an_array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def separateDigits(self, nums: List[int]) -> List[int]:
-        result = []
-        
-        for num in nums:
-            for digit in str(num):
-                result.append(int(digit))
-        
-        return result
