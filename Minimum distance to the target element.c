7f18fa116613f87274d3b419592b0fe0a98a1daf
Shreya_Pathak <Pathakshreyab@gmail.com>
2026-06-13 09:14:24 +0530

    Delete Minimum distance to the target element.c

--- Minimum distance to the target element.c
+++
@@ -1,9 +0,0 @@
-int getMinDistance(int* nums, int numsSize, int target, int start) {
-    int res = numsSize;
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] == target) {
-            res = fmin(res, abs(i - start));
-        }
-    }
-    return res;
-}
